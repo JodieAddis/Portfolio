b3f5a31f9a4c6bfebc02ce4dd34478959040056f
JodieAddis <jodie.addis.92@gmail.com>
2024-07-25 12:50:58 +0200

    correction hook

--- src/components/PortfolioContainer/index.tsx
+++ src/components/PortfolioContainer/index.tsx
@@ -7,7 +7,6 @@ import PresentationDesktop from "../Presentation/PresentationDesktop";
 import PresentationMobile from "../Presentation/PresentationMobile";
 import SectionProjects from "../Section/SectionProjects";
 import Skills from "../Skills";
-import useScreenSize from "../../hook/useScreenSize";
 
 const Component = () => {
   const isMobile = useScreenSize();
