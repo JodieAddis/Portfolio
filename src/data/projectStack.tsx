3ab04c1e08371648f9d508d456297203fc7db1a5
JodieAddis <jodie.addis.92@gmail.com>
2024-07-25 13:04:38 +0200

    correction : name path for images

--- src/data/projectStack.tsx
+++ src/data/projectStack.tsx
@@ -8,31 +8,32 @@ export const projectData: ProjectStackItem[] = [
   {
     name: "dev jobs",
     url: "#",
-    img: "../../public/assets/images/in-progress.webp",
+    // img: "../../public/assets/images/in-progress.webp",
+    img: "/public/assets/images/in-progress.webp",
   },
   {
     name: "planets fact site",
     url: "https://planets-fact-site-rosy-delta.vercel.app/",
-    img: "../../public/assets/images/planets-fact-site.webp",
+    img: "/public/assets/images/planets-fact-site.webp",
   },
   {
     name: "e-commerce sneakers",
     url: "https://jodieaddis.github.io/e-commerce-sneakers/",
-    img: "../../public/assets/images/e-commerce-sneakers.webp",
+    img: "/public/assets/images/e-commerce-sneakers.webp",
   },
   {
     name: "online dictionnary",
     url: "https://jodieaddis.github.io/online-dictionary/",
-    img: "../../public/assets/images/online-dictionary.webp",
+    img: "/public/assets/images/online-dictionary.webp",
   },
   {
     name: "crowFunding product page",
     url: "https://jodieaddis.github.io/crowdfunding_product_page/",
-    img: "../../public/assets/images/crowdfunding_product_page.webp",
+    img: "/public/assets/images/crowdfunding_product_page.webp",
   },
   {
     name: "password generator",
     url: "https://jodieaddis.github.io/Password-generator/",
-    img: "../../public/assets/images/password-generator.webp",
+    img: "/public/assets/images/password-generator.webp",
   },
 ];
