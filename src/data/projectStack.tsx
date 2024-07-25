b7e4badd82b316a68d8142f3130a16a5400307b0
Jodie <128384440+JodieAddis@users.noreply.github.com>
2024-07-25 13:17:53 +0200

    Update projectStack.tsx

--- src/data/projectStack.tsx
+++ src/data/projectStack.tsx
@@ -8,22 +8,22 @@ export const projectData: ProjectStackItem[] = [
   {
     name: "dev jobs",
     url: "#",
-    img: "/public/assets/images/in-progress.webp",
+    img: "../../public/assets/images/in-progress.webp",
   },
   {
     name: "planets fact site",
     url: "https://planets-fact-site-rosy-delta.vercel.app/",
-    img: "/public/assets/images/planets-fact-site.webp",
+    img: "../../public/assets/images/planets-fact-site.webp",
   },
   {
     name: "e-commerce sneakers",
     url: "https://jodieaddis.github.io/e-commerce-sneakers/",
-    img: "/public/assets/images/e-commerce-sneakers.webp",
+    img: "../../public/assets/images/e-commerce-sneakers.webp",
   },
   {
     name: "online dictionnary",
     url: "https://jodieaddis.github.io/online-dictionary/",
-    img: "/public/assets/images/online-dictionary.webp",
+    img: "../../public/assets/images/online-dictionary.webp",
   },
   {
     name: "crowFunding product page",
@@ -33,6 +33,6 @@ export const projectData: ProjectStackItem[] = [
   {
     name: "password generator",
     url: "https://jodieaddis.github.io/Password-generator/",
-    img: "/public/assets/images/password-generator.webp",
+    img: "../../public/assets/images/password-generator.webp",
   },
 ];
